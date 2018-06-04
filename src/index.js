1461e33d882ee26af78708a5cea52a25496bda88
Brady Hammond <bradyhammond@Bradys-MacBook-Pro-2017.local>
2018-06-04 17:16:17 -0600

    Finish Endpoints

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ import { parseSignatureHeader,
     getHistory,
     postHistory,
     getBlobs,
+    postBlobs,
+    putBlobs,
     getRelays,
+    postRelays,
+    putRelays,
+    deleteRelays,
     getErrors } from "./helping";
 
 // ================================================== //
@@ -30,12 +35,35 @@ console.log(postHistory("http://127.0.0.1:8080/",
         "signers":
             [
                 "Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=",
-                "Xq5YqaL6L48pf0fu7IUhL0JRaU2_RxFP0AL43wYn148="
+                "Xq5YqaL6L48pf0fu7IUhL0JRaU2_RxFP0AL43wYn148=",
             ]
-    }
-    ));
+    }));
 console.log(getHistory("http://127.0.0.1:8080/"));
+console.log(postBlobs("http://127.0.0.1:8080/",
+    'signer="AeYbsHot0pmdWAcgTo5sD8iAuSQAfnH5U6wiIGpVNJQQoYKBYrPPxAoIc1i5SHCIDS8KFFgf8i0tDq8XGizaCg=="',
+    {
+        "blob": "AeYbsHot0pmdWAcgTo5sD8iAuSQAfnH5U6wiIGpVNJQQoYKBYrPPxAoIc1i5SHCIDS8KFFgf8i0tDq8XGizaCgo9yjuKHHNJZFi0QD9K6Vpt6fP0XgXlj8z_4D-7s3CcYmuoWAh6NVtYaf_GWw_2sCrHBAA2mAEsml3thLmu50Dw",
+        "changed": "2000-01-01T00:00:00+00:00",
+        "id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE="
+    }));
+console.log(putBlobs("http://127.0.0.1:8080/",
+    'signer="AeYbsHot0pmdWAcgTo5sD8iAuSQAfnH5U6wiIGpVNJQQoYKBYrPPxAoIc1i5SHCIDS8KFFgf8i0tDq8XGizaCg=="',
+    {
+        "blob": "AeYbsHot0pmdWAcgTo5sD8iAuSQAfnH5U6wiIGpVNJQQoYKBYrPPxAoIc1i5SHCIDS8KFFgf8i0tDq8XGizaCgo9yjuKHHNJZFi0QD9K6Vpt6fP0XgXlj8z_4D-7s3CcYmuoWAh6NVtYaf_GWw_2sCrHBAA2mAEsml3thLmu50Dw",
+        "changed": "2001-01-01T00:00:00+00:00",
+        "id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE="
+    },
+    "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE="));
 console.log(getBlobs("http://127.0.0.1:8080/"));
+console.log(postRelays("http://127.0.0.1:8080/",
+    {
+        "changed": "2000-01-01T00:00:00+00:00",
+        "host_address": "127.0.0.1",
+        "main": true,
+        "name": "alpha",
+        "port": "7541"
+    }));
+console.log(deleteRelays("http://127.0.0.1:8080", "affa1c5ad6634ed3bc2d19c46152b96b"));
 console.log(getRelays("http://127.0.0.1:8080/"));
 console.log(getErrors("http://127.0.0.1:8080/"));
 
