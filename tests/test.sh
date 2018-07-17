3e93bade3913213594c0993b715df3aa7eda70e8
Brady Hammond <bradyhammond@Bradys-MacBook-Pro-2017.local>
2018-07-17 14:53:14 -0600

    Edit Test Script

--- tests/test.sh
+++ tests/test.sh
@@ -2,28 +2,21 @@
 
 set -o errexit
 
-before() {
-    git stash save 'Before Testing'
+test() {
+    git stash save 'Testing Backup'
     git clone https://github.com/reputage/didery.git
     cd didery
     git checkout dev
     cd ..
     pip3 install -e didery
-    dideryd -p 8080 .consensys/didery/db
-}
-
-test() {
-    npm tests
-}
-
-after() {
-    kill -INT 888
-    pip3 uninstall didery
+    dideryd .consensys/didery/db &
+    PID=$!
+    npm test
+    kill -9 $PID
+    pip3 uninstall -y didery
     rm -rf .consensys
     rm -rf didery
     git stash pop
 }
 
-before
 test
-after
