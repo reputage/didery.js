51b260c6f01fe3a86b4781e6d72a3261e2d882be
Brady Hammond <bradyhammond@Bradys-MacBook-Pro-2017.local>
2018-07-17 12:36:22 -0600

    Edit Test Script

--- tests/test.sh
+++ tests/test.sh
@@ -3,8 +3,10 @@
 set -o errexit
 
 git stash save 'Before Testing'
-pip3 install -r requirements.txt
+pip3 install -r tests/requirements.txt
 virtualenv .virtualenvs
+source .virtualenvs/bin/activate
 #git clone https://github.com/reputage/didery.js.git
 #git checkout dev
+deactivate
 git stash pop
